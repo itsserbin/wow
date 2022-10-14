01f4dc661254d5d208178db67605abfb3d6cc842
itsserbin <serbin.official@gmail.com>
2022-10-15 02:21:55 +0300

    up

--- resources/views/components/advantages.blade.php
+++ resources/views/components/advantages.blade.php
@@ -2,10 +2,10 @@
     <section class="bg-black grid grid-cols-1 md:grid-cols-3 mb-3 p-16 rounded-full">
         @foreach($advantages as $advantage)
             <div class="text-center text-white md:mb-0 mb-7">
-                <div class="flex justify-center">
+                <div class="flex justify-center mb-3">
                     {!! $advantage->icon !!}
                 </div>
-                <h3 class="">
+                <h3 class="font-bold">
                     {{app()->getLocale() == 'ua' ? $advantage->text['ua'] : $advantage->text['ru'] }}
                 </h3>
             </div>
