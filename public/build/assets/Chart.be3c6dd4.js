679018a2e896d416076fa663272318403b9052ca
itsserbin <serbin.official@gmail.com>
2022-10-15 18:43:44 +0300

    up

---
+++ public/build/assets/Chart.be3c6dd4.js
@@ -0,0 +1,1 @@
+import{L as r}from"./index.22e717de.js";import{u as e,U as o,O as s,Q as n}from"./_plugin-vue_export-helper.492f51f3.js";import"./app.474ab436.js";const m={__name:"Chart",props:["chartData"],setup(t){const a=e({responsive:!0,interaction:{intersect:!1}});return(c,i)=>(n(),o(s(r),{chartData:t.chartData,options:a},null,8,["chartData","options"]))}};export{m as default};
