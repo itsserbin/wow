9caea31a52eb86b58208f0206a52e2fd02f86beb
itsserbin <serbin.official@gmail.com>
2022-10-16 21:04:56 +0300

    up

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,8 +8,10 @@
     <title>@yield('title')</title>
     @vite('resources/scss/public.scss')
     @routes
+    {!! $options['head_scripts'] !!}
 </head>
 <body>
+{!! $options['after_body_scripts'] !!}
 <div id="app">
     <header class="w-[100%] fixed top-0 z-10 bg-[#fff]">
         <header-component
@@ -40,5 +42,35 @@
     @include('components.footer')
 </div>
 @vite('resources/js/Pages/Public/app.js')
+@if(env('APP_ENV') !== 'local')
+    <script>
+        !function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', {{env('FB_PIXEL_ID')}});
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none"
+             src="'https://www.facebook.com/tr?id={{env('FB_PIXEL_ID')}}&ev=PageView&noscript=1'"
+        />
+    </noscript>
+@endif
+{!! $options['footer_scripts'] !!}
 </body>
 </html>
