e06cb79a4ad191ddbd6a19b1d2b69537360737ef
itsserbin <serbin.official@gmail.com>
2022-10-19 15:20:06 +0300

    up

--- app/Console/Commands/OrdersStatisticCommand.php
+++ app/Console/Commands/OrdersStatisticCommand.php
@@ -24,8 +24,8 @@ class OrdersStatisticCommand extends Command
      */
     protected $description = 'Command description';
 
-    private $ordersRepository;
-    private $ordersStatisticsRepository;
+    private mixed $ordersRepository;
+    private mixed $ordersStatisticsRepository;
 
     /**
      * Create a new command instance.
@@ -53,23 +53,7 @@ class OrdersStatisticCommand extends Command
         if (!$statisticsNow) {
             $model = $this->ordersStatisticsRepository->createNewModel();
             $model->date = $dateNow;
-            $model->applications = $this->ordersRepository->countOrdersByStatus(null, $dateNow);
-            $model->unprocessed = $this->ordersRepository->countOrdersByStatus('new', $dateNow);
-            $model->completed = $this->ordersRepository->countOrdersByStatus('done', $dateNow);
-            $model->refunds = $this->ordersRepository->countOrdersByStatus('return', $dateNow);
-            $model->cancel = $this->ordersRepository->countOrdersByStatus('canceled', $dateNow);
-            $model->at_the_post_office = $this->ordersRepository->countOrdersByStatus('at_the_post_office', $dateNow);
-            $model->in_process = $this->ordersRepository->countOrdersByStatus('processed', $dateNow);
-            $model->transferred_to_supplier = $this->ordersRepository->countOrdersByStatus('transferred_to_supplier', $dateNow);
-            $model->awaiting_dispatch = $this->ordersRepository->countOrdersByStatus('awaiting_dispatch', $dateNow);
-            $model->awaiting_prepayment = $this->ordersRepository->countOrdersByStatus('awaiting_prepayment', $dateNow);
-            $model->on_the_road = $this->ordersRepository->countOrdersByStatus('on_the_road', $dateNow);
-            $model->requires_clarification = $this->ordersRepository->countOrdersByStatus('requires_clarification', $dateNow);
-            if ($model->applications) {
-                $model->canceled_orders_rate = $model->cancel / $model->applications;
-                $model->returned_orders_ratio = $model->refunds / $model->applications;
-                $model->received_parcel_ratio = $model->completed / $model->applications;
-            }
+
             $model->save();
         }
 
@@ -77,21 +61,21 @@ class OrdersStatisticCommand extends Command
 
         foreach ($statisticsAll as $item) {
             $item->applications = $this->ordersRepository->countOrdersByStatus(null, $item->date);
-            $item->unprocessed = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_NEW, $item->date);
-            $item->completed = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_DONE, $item->date);
-            $item->refunds = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_RETURN, $item->date);
-            $item->cancel = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_CANCELED, $item->date);
-            $item->at_the_post_office = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_AT_THE_POST_OFFICE, $item->date);
-            $item->in_process = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_PROCESSED, $item->date);
-            $item->transferred_to_supplier = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_TRANSFERRED_TO_SUPPLIER, $item->date);
-            $item->awaiting_dispatch = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_AWAITING_DISPATCH, $item->date);
-            $item->awaiting_prepayment = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_AWAITING_PREPAYMENT, $item->date);
-            $item->on_the_road = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_ON_THE_ROAD, $item->date);
-            $item->requires_clarification = $this->ordersRepository->countOrdersByStatus(OrderStatus::STATUS_REQUIRES_CLARIFICATION, $item->date);
+            $item->unprocessed = $this->ordersRepository->countOrdersByStatus('new', $item->date);
+            $item->completed = $this->ordersRepository->countOrdersByStatus('done', $item->date);
+            $item->refunds = $this->ordersRepository->countOrdersByStatus('return', $item->date);
+            $item->cancel = $this->ordersRepository->countOrdersByStatus('canceled', $item->date);
+            $item->at_the_post_office = $this->ordersRepository->countOrdersByStatus('at_the_post_office', $item->date);
+            $item->in_process = $this->ordersRepository->countOrdersByStatus('processed', $item->date);
+            $item->transferred_to_supplier = $this->ordersRepository->countOrdersByStatus('transferred_to_supplier', $item->date);
+            $item->awaiting_dispatch = $this->ordersRepository->countOrdersByStatus('awaiting_dispatch', $item->date);
+            $item->awaiting_prepayment = $this->ordersRepository->countOrdersByStatus('awaiting_prepayment', $item->date);
+            $item->on_the_road = $this->ordersRepository->countOrdersByStatus('on_the_road', $item->date);
+            $item->requires_clarification = $this->ordersRepository->countOrdersByStatus('requires_clarification', $item->date);
             if ($item->applications) {
-                $item->canceled_orders_rate = ($item->cancel / $item->applications) * 100;
-                $item->returned_orders_ratio = ($item->refunds / $item->applications) * 100;
-                $item->received_parcel_ratio = ($item->completed / $item->applications) * 100;
+                $item->canceled_orders_rate = $item->cancel / $item->applications;
+                $item->returned_orders_ratio = $item->refunds / $item->applications;
+                $item->received_parcel_ratio = $item->completed / $item->applications;
             }
             $item->update();
         }
