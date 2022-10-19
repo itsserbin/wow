6f26d0edd1a06d840078e2f74beca08710854fe5
itsserbin <serbin.official@gmail.com>
2022-10-19 03:37:09 +0300

    up

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,6 +23,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\LocaleMiddleware::class,
         \App\Http\Middleware\SecuredHttp::class,
+        \App\Http\Middleware\RedirectToNonWwwMiddleware::class,
     ];
 
     /**
@@ -39,7 +40,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
-            \App\Http\Middleware\RedirectToNonWwwMiddleware::class,
         ],
 
         'api' => [
