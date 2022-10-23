3aabe119adae17ca6e06dc28d76244c4b74283c0
itsserbin <serbin.official@gmail.com>
2022-10-23 20:22:36 +0300

    up

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.master')
 
+@section('scripts')
+    <script id="widget-wfp-script" language="javascript" type="text/javascript"
+            src="https://secure.wayforpay.com/server/pay-widget.js"></script>
+@endsection
+
 @section('content')
     {{ Breadcrumbs::render('home.checkout') }}
 
     <div class="checkout">
-        
+
         <checkout-component></checkout-component>
-   
+
     </div>
 @endsection
