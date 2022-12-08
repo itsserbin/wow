c0aa969cb335c64006980ab6bb6abd2e39869691
itsserbin <serbin.official@gmail.com>
2022-12-09 00:21:48 +0200

    up

--- resources/js/Pages/Admin/Options/OptionsLayout.vue
+++ resources/js/Pages/Admin/Options/OptionsLayout.vue
@@ -8,12 +8,14 @@
             <div class="md:col-span-1">
                 <sidebar-component class="mb-5">
                     <sidebar-item v-for="item in items"
+                                  v-if="can(item.permission)"
                                   :item="item"
                                   :active="route(route().current()) === item.href"
                     />
                 </sidebar-component>
                 <sidebar-component class="mb-5">
                     <sidebar-item v-for="item in items3"
+                                  v-if="can(item.permission)"
                                   :item="item"
                                   :active="route(route().current()) === item.href"
                     />
@@ -21,6 +23,7 @@
 
                 <sidebar-component>
                     <sidebar-item v-for="item in items2"
+                                  v-if="can(item.permission)"
                                   :item="item"
                                   :active="route(route().current()) === item.href"
                     />
@@ -36,74 +39,87 @@
 </template>
 
 <script setup>
-import Sidebar from '@/Components/Sidebar/Sidebar.vue';
+import {inject} from "vue";
 
-import {reactive, ref} from "vue";
-
+const can = inject('$can');
 defineProps(['title']);
 
-const items = reactive([
+const items = [
     {
         title: 'Головна',
+        permission: 'show-options',
         href: route('admin.options.index')
     },
     {
         title: 'Основні',
+        permission: 'show-main-options',
         href: route('admin.options.main')
     },
     {
         title: 'Скрипти',
+        permission: 'show-scripts-options',
         href: route('admin.options.scripts')
     },
     {
         title: 'Банери',
+        permission: 'show-banners',
         href: route('admin.options.banners.index')
     },
     {
         title: 'Переваги',
+        permission: 'show-advantages',
         href: route('admin.options.advantages.index')
     },
     {
         title: 'FAQ',
+        permission: 'show-faqs',
         href: route('admin.options.faqs.index')
     },
-]);
+];
 
-const items2 = reactive([
+const items2 = [
     {
         title: 'Користувачі',
+        permission: 'show-users',
         href: route('admin.users.index')
     },
     {
         title: 'Ролі',
+        permission: 'show-roles',
         href: route('admin.roles.index')
     },
     {
         title: 'Права',
+        permission: 'show-permissions',
         href: route('admin.permissions.index')
     }
-]);
+];
 
-const items3 = reactive([
+const items3 = [
     {
         title: 'Кольори',
+        permission: 'show-colors',
         href: route('admin.options.colors.index')
     },
     {
         title: 'Розміри',
+        permission: 'show-sizes',
         href: route('admin.options.sizes.index')
     },
     {
         title: 'Постачальники',
+        permission: 'show-providers',
         href: route('admin.options.providers.index')
     },
     {
         title: 'Промо-коди',
+        permission: 'show-promo-codes',
         href: route('admin.options.promo-codes.index')
     },
     {
         title: 'XML',
+        permission: 'show-xmls',
         href: route('admin.options.xmls.index')
     },
-]);
+];
 </script>
