41c4ec1a391dd1f102283526b35014f6af30eb53
itsserbin <serbin.official@gmail.com>
2023-01-03 18:56:59 +0200

    up

--- resources/js/Pages/Public/Product/Content.vue
+++ resources/js/Pages/Public/Product/Content.vue
@@ -1,29 +1,28 @@
 <template>
-    <div class="border-b border-red-200 dark:border-red-700">
-        <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-red-500 dark:text-red-400">
-            <li class="mr-2" v-for="item in state" @click="setActiveTab(item.key)">
-                <a href="javascript:"
-                   class="
+    <div class="grid grid-cols-1 gap-4">
+        <div class="border-b border-red-200 dark:border-red-700">
+            <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-red-500 dark:text-red-400">
+                <li class="mr-2" v-for="item in items" @click="setActiveTab(item.key,item.value,item.class)">
+                    <a href="javascript:"
+                       class="
                             inline-flex
                             p-4
                             border-b-2
                             border-transparent
                             rounded-t-lg
                             hover:text-red-600
-                            hover:border-red-300
-                            dark:hover:text-red-300
+                            hover:border-red-600
+                            dark:hover:text-red-600
                             group
                         "
-                   :class="{'text-red-600' : active === item.key}"
-                >
-                    {{ item.label }}
-                </a>
-            </li>
-        </ul>
-    </div>
-    <div v-for="item in state" :class="item.class">
-        <div
-            v-html="active === item.key ? (active === 'sizeTable' ? item.value : (lang === 'ru' ? item.value.ru : item.value.ua )) : null"></div>
+                       :class="{'text-red-600 border-red-600' : state.active === item.key}"
+                    >
+                        {{ item.label }}
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div v-html="state.content" :class="state.class"></div>
     </div>
 </template>
 
@@ -42,9 +41,14 @@ const props = defineProps([
     'deliveryAndPaymentRu',
 ]);
 
-const active = ref('characteristics');
-
-const state = [
+const state = ref({
+    active: 'characteristics',
+    content: props.lang === 'ru' ? JSON.parse(props.characteristics).ru : JSON.parse(props.characteristics).ua,
+    class: 'characteristics-table'
+})
+// const active = ref('characteristics');
+// const content = ref(JSON.parse(props.characteristics));
+const items = [
     {
         label: 'Характеристики',
         key: 'characteristics',
@@ -77,7 +81,11 @@ const state = [
     },
 ];
 
-function setActiveTab(val) {
-    active.value = val;
+function setActiveTab(key, val, style) {
+    state.value.active = key;
+    state.value.content = key === 'sizeTable' ? val : (props.lang === 'ru' ? val.ru : val.ua);
+    state.value.class = style;
+    // active.value = key;
+    // content.value = props.lang === 'ru' ? val.ru : val.ua;
 }
 </script>
