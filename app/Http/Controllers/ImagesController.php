c45355cd504010ee06f063338ae4aa50b5b6a639
itsserbin <serbin.official@gmail.com>
2023-01-08 04:52:23 +0200

    up

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -10,57 +10,48 @@ use Throwable;
 
 class ImagesController extends Controller
 {
-    public function mobileBanner($filename)
+    public function mobileBanner($filename): StreamedResponse
     {
         return $this->returnImage(ImagesPath::MOBILE_BANNER, $filename);
     }
 
-    public function tableBanner($filename)
+    public function tableBanner($filename): StreamedResponse
     {
         return $this->returnImage(ImagesPath::TABLE_BANNER, $filename);
     }
 
-    public function desktopBanner($filename)
+    public function desktopBanner($filename): StreamedResponse
     {
         return $this->returnImage(ImagesPath::DESKTOP_BANNER, $filename);
     }
 
-    public function products($filename)
+    public function products($filename): StreamedResponse
     {
         return $this->returnImage(ImagesPath::PRODUCT_IMAGE, $filename);
     }
 
-    public function products55($filename)
+    public function products55($filename): StreamedResponse
     {
         return $this->returnImage(ImagesPath::PRODUCT_IMAGE_55, $filename);
     }
 
-    public function products350($filename)
+    public function products350($filename): StreamedResponse
     {
         return $this->returnImage(ImagesPath::PRODUCT_IMAGE_350, $filename);
     }
 
-    public function products500($filename)
+    public function products500($filename): StreamedResponse
     {
         return $this->returnImage(ImagesPath::PRODUCT_IMAGE_500, $filename);
     }
 
-    public function category($filename)
+    public function category($filename): StreamedResponse
     {
         return $this->returnImage(ImagesPath::CATEGORY, $filename);
     }
 
-    public function returnImage($path, $filename)
+    public function returnImage($path, $filename): StreamedResponse
     {
         return Storage::disk('s3')->response($path . $filename);
-//        try {
-//        } catch (Throwable $e) {
-//            try {
-//                return Storage::disk('s3')->response($path . urldecode($filename));
-//            } catch (Throwable $e) {
-//                Log::error('img' . $e);
-//            }
-//            Log::error('img' . $e);
-//        }
     }
 }
