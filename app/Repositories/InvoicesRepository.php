77348e32bb60ab3ce8d8914189d0380bca7987a2
itsserbin <serbin.official@gmail.com>
2023-01-17 13:51:24 +0200

    up

--- app/Repositories/InvoicesRepository.php
+++ app/Repositories/InvoicesRepository.php
@@ -58,6 +58,7 @@ class InvoicesRepository extends CoreRepository
 
         $item = $this->model::where('id', $model->id)->with('order.client', 'order.items.product')->first();
 
+        dd($model->id);
         $names = [];
         $prices = [];
         $counts = [];
