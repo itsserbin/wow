06726944060f19a95164a10f7badaa7992a22a05
itsserbin <serbin.official@gmail.com>
2023-01-24 18:00:30 +0200

    up

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ Route::group(['prefix' => App\Http\Middleware\LocaleMiddleware::getLocale()], fu
 });
 
 Route::get('test', function () {
-    return dd($_SERVER);
+    return dd($_SERVER['HTTP_X_REAL_IP']);
 });
 
 Route::post('sms-new-order', [SmsController::class, 'newOrder'])
