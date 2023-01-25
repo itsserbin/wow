1a722305d05d1660f97f218b53aaa510023edd6a
itsserbin <serbin.official@gmail.com>
2023-01-26 01:24:04 +0200

    up

--- resources/js/Pages/Public/Components/ScrollToTop.vue
+++ resources/js/Pages/Public/Components/ScrollToTop.vue
@@ -4,7 +4,7 @@
             class="
                     cursor-pointer
                     items-center
-                    z-1
+                    z-10
                     bg-secondary
                     hover:bg-button
                     rounded-[50%]
@@ -17,18 +17,17 @@
                     duration-500
             "
     >
-        <span class="icon-arrow-up2 ">
-        <svg xmlns="http://www.w3.org/2000/svg"
-             width="16"
-             height="16"
-             fill="currentColor"
-             class="bi bi-arrow-up mx-auto"
-             viewBox="0 0 16 16"
-        >
-                <path fill-rule="evenodd"
-                      d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-            </svg>
-    </span>
+        <!--        <span class="icon-arrow-up2 ">-->
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                      d="M17.5303 15.0303C17.2374 15.3232 16.7626 15.3232 16.4697 15.0303L12 10.5607L7.53033 15.0303C7.23744 15.3232 6.76256 15.3232 6.46967 15.0303C6.17678 14.7374 6.17678 14.2626 6.46967 13.9697L11.4697 8.96967C11.6103 8.82902 11.8011 8.75 12 8.75C12.1989 8.75 12.3897 8.82902 12.5303 8.96967L17.5303 13.9697C17.8232 14.2626 17.8232 14.7374 17.5303 15.0303Z"
+                      fill="#363853"></path>
+            </g>
+        </svg>
+        <!--    </span>-->
     </button>
 </template>
 
