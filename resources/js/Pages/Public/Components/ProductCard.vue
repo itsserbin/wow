56a8d7ceaef6fba2576a5261f4324ffe1a61ef4f
itsserbin <serbin.official@gmail.com>
2023-01-26 02:11:39 +0200

    up

--- resources/js/Pages/Public/Components/ProductCard.vue
+++ resources/js/Pages/Public/Components/ProductCard.vue
@@ -342,6 +342,10 @@ function addToCard(id) {
     /*background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 17.00 17.00' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' class='si-glyph si-glyph-arrow-left' fill='none'%3E%3Cg stroke-width='0'%3E%3C/g%3E%3Cg stroke-linecap='round' stroke-linejoin='round'%3E%3C/g%3E%3Cg%3E%3Cdefs%3E%3C/defs%3E%3Cg stroke-width='0.00017' fill='none'%3E%3Cpath d='M10.978,1.162 C10.978,1.387 10.916,1.612 10.782,1.812 L6.626,8.041 L10.823,14.078 C11.182,14.619 11.036,15.348 10.495,15.707 C9.956,16.068 9.224,15.922 8.865,15.382 L4.235,8.694 C3.971,8.3 3.969,7.786 4.233,7.39 L8.822,0.51 C9.182,-0.032 9.914,-0.178 10.454,0.181 C10.795,0.409 10.978,0.782 10.978,1.162 L10.978,1.162 Z' fill='%23b8b8b8' fill-opacity='0.3' class='si-glyph-fill'%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/svg%3E") !important*/
 }
 
+.swiper-button-next, .swiper-button-prev {
+    text-rendering: auto;
+}
+
 /*.swiper-button-prev::after,*/
 /*.swiper-button-next::after {*/
 /*    display: none;*/
