46044b71f0047d0720745d1ae8b720b5deef7ac3
itsserbin <serbin.official@gmail.com>
2023-01-26 06:03:31 +0200

    up

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
+    <title>@yield('meta_title')</title>
     <meta name="description" content="@yield('meta_description')">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     @vite('resources/scss/public.scss')
