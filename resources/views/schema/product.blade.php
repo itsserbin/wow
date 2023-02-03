6447efd109e74e343ceaa392129d0920f3a2a98c
itsserbin <serbin.official@gmail.com>
2023-02-03 20:44:23 +0200

    up

--- resources/views/schema/product.blade.php
+++ resources/views/schema/product.blade.php
@@ -9,32 +9,34 @@
       "sku": "{{$product->id}}",
       "name": "{{$product->h1[app()->getLocale()]}}",
       "description": "{{$product->description[app()->getLocale()]}}",
-      "image": "{{route('images',$product->preview->src)}}",
-      "review": [
-      @foreach($product->reviews as $review)
-            {
-            "@type": "Review",
-            "reviewRating": {
-              "@type": "Rating",
-              "ratingValue": "5"
-            },
-            "author": {
-              "@type": "Person",
-              "name": "{{$review->name}}"
-            }
-        @if($loop->last)
-            }
-        @else
-            },
-        @endif
-      @endforeach
-      ],
-      "aggregateRating": {
-        "@type": "AggregateRating",
-        "ratingValue": "5",
-        "bestRating": "5",
-        "reviewCount": "{{count($product->reviews)}}"
-      }
+      "image": "{{route('images',$product->preview->src)}}"
+      @if(count($product->reviews)),
+          "review": [
+          @foreach($product->reviews as $review)
+                {
+                "@type": "Review",
+                "reviewRating": {
+                  "@type": "Rating",
+                  "ratingValue": "5"
+                },
+                "author": {
+                  "@type": "Person",
+                  "name": "{{$review->name}}"
+                }
+            @if($loop->last)
+                }
+            @else
+                },
+            @endif
+          @endforeach
+          ],
+          "aggregateRating": {
+            "@type": "AggregateRating",
+            "ratingValue": "5",
+            "bestRating": "5",
+            "reviewCount": "{{count($product->reviews)}}"
+          }
+      @endif
     }
 
 </script>
