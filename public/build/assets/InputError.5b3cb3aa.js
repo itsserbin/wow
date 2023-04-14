38dc7e8b8c75bf0e6c51b79d82331a34fef9cec8
itsserbin <serbin.official@gmail.com>
2023-04-14 03:40:33 +0300

    up

---
+++ public/build/assets/InputError.5b3cb3aa.js
@@ -0,0 +1,1 @@
+import{z as s,E as t,o as a,i as r,j as o,t as c}from"./vendor.2c8aab53.js";const n={class:"text-sm text-red-600"},p={__name:"InputError",props:["message"],setup(e){return(i,m)=>s((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{p as _};
