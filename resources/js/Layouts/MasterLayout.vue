40d47dcf2ba0ef583e30373c7cd28a2c3ddc5021
itsserbin <serbin.official@gmail.com>
2023-04-20 05:33:58 +0300

    up

--- resources/js/Layouts/MasterLayout.vue
+++ resources/js/Layouts/MasterLayout.vue
@@ -18,7 +18,7 @@ onMounted(() => {
         <FixedBanner :lang="$page.props.lang"/>
         <Header :options="$page.props.options" :categories="$page.props.categories" :lang="$page.props.lang"/>
     </header>
-    <main class="max-w-7xl mx-auto pt-36 md:pt-40 px-3 h-full flex-[1_0_auto]">
+    <main class="max-w-7xl mx-auto pt-36 md:pt-40 px-3 h-full flex flex-[1_0_auto]">
         <slot></slot>
     </main>
     <Footer :options="$page.props.options" :lang="$page.props.lang" :pages="$page.props.pages"/>
