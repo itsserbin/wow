a28f81b56b2b6ae459dda82c16cced79be89889b
itsserbin <serbin.official@gmail.com>
2023-04-20 06:38:06 +0300

    up

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,14 +30,14 @@
 </head>
 <body class="h-full flex flex-col">
 {{--{!! $options['after_body_scripts'] !!}--}}
-<div class="loader fixed inset-0 z-50 flex items-center justify-center backdrop-filter backdrop-blur-md">
-    <div class="lds-ellipsis inline-block relative w-[80px] h-[80px]">
-        <div class="absolute top-[33px] w-[13px] h-[13px] rounded-[50%] bg-[#ff0000]"></div>
-        <div class="absolute top-[33px] w-[13px] h-[13px] rounded-[50%] bg-[#ff0000]"></div>
-        <div class="absolute top-[33px] w-[13px] h-[13px] rounded-[50%] bg-[#ff0000]"></div>
-        <div class="absolute top-[33px] w-[13px] h-[13px] rounded-[50%] bg-[#ff0000]"></div>
-    </div>
-</div>
+{{--<div class="loader fixed inset-0 z-50 flex items-center justify-center backdrop-filter backdrop-blur-md">--}}
+{{--    <div class="lds-ellipsis inline-block relative w-[80px] h-[80px]">--}}
+{{--        <div class="absolute top-[33px] w-[13px] h-[13px] rounded-[50%] bg-[#ff0000]"></div>--}}
+{{--        <div class="absolute top-[33px] w-[13px] h-[13px] rounded-[50%] bg-[#ff0000]"></div>--}}
+{{--        <div class="absolute top-[33px] w-[13px] h-[13px] rounded-[50%] bg-[#ff0000]"></div>--}}
+{{--        <div class="absolute top-[33px] w-[13px] h-[13px] rounded-[50%] bg-[#ff0000]"></div>--}}
+{{--    </div>--}}
+{{--</div>--}}
 @inertia
 {{--    <master-layout>--}}
 {{--        <header class="w-[100%] fixed top-0 z-20 bg-[#fff] flex-[0_0_auto]">--}}
