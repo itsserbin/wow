7a7fc9b1a607b87fa592eec255eff6860725136d
itsserbin <serbin.official@gmail.com>
2023-05-12 22:13:37 +0300

    up

--- resources/js/Components/Sidebar/SidebarItem.vue
+++ resources/js/Components/Sidebar/SidebarItem.vue
@@ -7,6 +7,7 @@ defineProps(['item', 'active']);
 <template>
     <li>
         <Link
+            v-if="item"
             :href="item.href ? item.href : 'javascript:'"
             :class="{'bg-gray-100 dark:bg-gray-700' : active}"
             class="
@@ -23,8 +24,26 @@ defineProps(['item', 'active']);
             "
         >
             <i v-if="item.icon" :class="item.icon"></i>
-
             <span class="ml-3">{{ item.title }}</span>
         </Link>
+        <a v-else
+           href="javascript:"
+           :class="{'bg-gray-100 dark:bg-gray-700' : active}"
+           class="
+                flex
+                items-center
+                p-2
+                text-base
+                font-normal
+                text-gray-900
+                rounded-lg
+                dark:text-white
+                hover:bg-gray-100
+                dark:hover:bg-gray-700
+            "
+        >
+            <slot name="icon"></slot>
+            <slot></slot>
+        </a>
     </li>
 </template>
